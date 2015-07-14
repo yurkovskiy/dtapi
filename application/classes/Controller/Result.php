8e4c03ecc119c0c2b5aa9dcd5f71e357c601a845
yurkovskiy <yurkovskiy@gmail.com>
2015-07-14 14:11:28 +0300

    Added Result Entity

---
+++ application/classes/Controller/Result.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Result Controller for handle AJAX requests
+ *
+ */
+
+class Controller_Result extends Controller_BaseAjax {
+
+	protected $modelName = "Result";
+
+}
