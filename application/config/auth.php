7817c68759b7c2b0294ae001ed0d97ace48baeef
yurkovskiy <yurkovskiy@gmail.com>
2015-07-17 13:16:40 +0300

    Auth module added

---
+++ application/config/auth.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+
+	'driver'       => 'orm',
+	'hash_method'  => 'sha256',
+	'hash_key'     => 'tuhes',
+	'lifetime'     => 1209600,
+	'session_type' => Session::$default,
+	'session_key'  => 'auth_user',
+
+);
