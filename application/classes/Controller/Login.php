99af4de80d412933f28353d1479fb69fb1d980ea
yurkovskiy <yurkovskiy@gmail.com>
2015-07-17 14:52:02 +0300

    Modified Login Controller, added data reading from JSON

--- application/classes/Controller/Login.php
+++ application/classes/Controller/Login.php
@@ -6,11 +6,24 @@
  */
 
 class Controller_Login extends Controller {
+	
+	private $RAW_DATA_SOURCE = "php://input";
 
 	public function action_index()
 	{
-		$sucsses = Auth::instance()->login("admin", "dtapi_admin", FALSE);
-		var_dump($sucsses);
+		$result = array();
+		$params = json_decode(file_get_contents($this->RAW_DATA_SOURCE));
+		$succsses = Auth::instance()->login($params->username, $params->password, FALSE);
+		if ($succsses)
+		{
+			$result["response"] = "ok";
+		}
+		else 
+		{
+			$result["response"] = "Invalid login or password";
+		}
+		
+		$this->response->body(json_encode($result));
 	}
 	
 	public function action_logout()
--- application/logs/2015/07/17.php
+++ application/logs/2015/07/17.php
@@ -139,3 +139,12 @@
 #12 /var/www/dtapi/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
 #13 /var/www/dtapi/index.php(118): Kohana_Request->execute()
 #14 {main} in /var/www/dtapi/modules/orm/classes/Kohana/ORM.php:1668
+2015-07-17 06:49:02 --- EMERGENCY: ErrorException [ 8 ]: Undefined variable: sucsses ~ APPPATH/classes/Controller/Login.php [ 18 ] in /var/www/dtapi/application/classes/Controller/Login.php:18
+2015-07-17 06:49:02 --- DEBUG: #0 /var/www/dtapi/application/classes/Controller/Login.php(18): Kohana_Core::error_handler(8, 'Undefined varia...', '/var/www/dtapi/...', 18, Array)
+#1 /var/www/dtapi/system/classes/Kohana/Controller.php(84): Controller_Login->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /var/www/dtapi/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Login))
+#4 /var/www/dtapi/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /var/www/dtapi/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#6 /var/www/dtapi/index.php(118): Kohana_Request->execute()
+#7 {main} in /var/www/dtapi/application/classes/Controller/Login.php:18
