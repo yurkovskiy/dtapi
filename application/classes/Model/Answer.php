29e1eb9562ab8f30778f9001a32818af00af0e9c
yurkovskiy <yurkovskiy@gmail.com>
2015-07-29 18:34:29 +0300

    Added new methods for return answers of some question by question_id

--- application/classes/Model/Answer.php
+++ application/classes/Model/Answer.php
@@ -10,4 +10,11 @@ class Model_Answer extends Model_Common {
 	protected $tableName = "answers";
 	protected $fieldNames = array("answer_id","question_id", "true_answer", "answer_text", "attachment");
 	
+	public function getAnswersByQuestion($question_id)
+	{
+		$query = DB::select_array($this->fieldNames)->from($this->tableName)->where($this->fieldNames[1], "=", $question_id)->order_by($this->fieldNames[0], 'asc');
+		$result = $query->as_object()->execute();
+		return $result;
+	}
+	
 }
