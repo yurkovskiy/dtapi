f60d73810482ca5450e295bef5be6eab65f5a5e7
yurkovskiy <yurkovskiy@gmail.com>
2015-07-30 07:49:49 +0300

    Fixed bug with speciality. Added getTestsBySubject() and getAnswersByQuestion() methods

--- application/classes/Model/Test.php
+++ application/classes/Model/Test.php
@@ -10,4 +10,11 @@ class Model_Test extends Model_Common {
 	protected $tableName = "tests";
 	protected $fieldNames = array("test_id", "test_name","subject_id", "tasks", "time_for_test", "enabled", "attempts");
 	
+	public function getTestsBySubject($subject_id)
+	{
+		$query = DB::select_array($this->fieldNames)->from($this->tableName)->where($this->fieldNames[2], "=", $subject_id)->order_by($this->fieldNames[0], 'asc');
+		$result = $query->as_object()->execute();
+		return $result;
+	}
+	
 }
