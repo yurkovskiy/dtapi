e5d2075bc74f0d5ed9c2eeb58fff726a8e4183e4
yurkovskiy <yurkovskiy@gmail.com>
2015-08-18 13:09:36 +0300

    Added functionality for pagination in Question Controller/Model

--- application/classes/Model/Question.php
+++ application/classes/Model/Question.php
@@ -10,6 +10,13 @@ class Model_Question extends Model_Common {
 	protected $tableName = "questions";
 	protected $fieldNames = array("question_id", "test_id", "question_text", "level", "type", "attachment");
 	
+	/**
+	 * 
+	 * @param int $test_id
+	 * @param int $level
+	 * @param int $number
+	 * @return MySQL Result Set
+	 */
 	public function getQuestionsByLevelRand($test_id, $level, $number)
 	{
 		$query = DB::select_array($this->fieldNames)->from($this->tableName)
@@ -20,5 +27,38 @@ class Model_Question extends Model_Common {
 		$result = $query->as_object()->execute();
 		return $result;
 	}
+	
+	/**
+	 * 
+	 * @param int $test_id
+	 * @return int $count - number of records
+	 */
+	public function countQuestionsByTest($test_id)
+	{
+		$query = "SELECT COUNT(*) AS count FROM {$this->tableName} WHERE {$this->fieldNames[1]} = {$test_id}";
+		$count = DB::query(Database::SELECT, $query)->execute()->get('count');
+		return $count;
+	}
+	
+	/**
+	 * 
+	 * @param int $test_id
+	 * @param int $limit
+	 * @param int $offset
+	 * @return MySQL Result Set
+	 */
+	public function getQuestionsRangeByTest($test_id, $limit, $offset)
+	{
+		$query = DB::select_array($this->fieldNames)
+				->from($this->tableName)
+				->where($this->fieldNames[1], "=", $test_id)
+				->order_by($this->fieldNames[0], 'asc')
+				->limit($limit)
+				->offset($offset);
+		$result = $query->as_object()->execute();
+		return $result;
+	}
+	
+	
 	
 }
