9399ebc9c806a31619d7b9341ff8861b636f43dc
yurkovskiy <yurkovskiy@gmail.com>
2015-08-19 12:50:43 +0300

    Added getTestDetailsByTest functionality into TestDetail Controller/Model

--- application/classes/Model/TestDetail.php
+++ application/classes/Model/TestDetail.php
@@ -10,4 +10,11 @@ class Model_TestDetail extends Model_Common {
 	protected $tableName = "test_details";
 	protected $fieldNames = array("id","test_id", "level", "tasks", "rate");
 	
+	public function getTestDetailsByTest($test_id)
+	{
+		$query = DB::select_array($this->fieldNames)->from($this->tableName)->where($this->fieldNames[1], "=", $test_id)->order_by($this->fieldNames[0], 'asc');
+		$result = $query->as_object()->execute();
+		return $result;
+	}
+	
 }
