367dec8357a59e00c4b2534402091c11746ee9b9
yurkovskiy <yurkovskiy@gmail.com>
2015-08-19 12:50:43 +0300

    Added getTestDetailsByTest functionality into TestDetail Controller/Model

--- application/classes/Controller/TestDetail.php
+++ application/classes/Controller/TestDetail.php
@@ -8,5 +8,28 @@
 class Controller_TestDetail extends Controller_BaseAdmin {
 
 	protected $modelName = "TestDetail";
+	
+	public function action_getTestDetailsByTest()
+	{
+		$record_id = $this->request->param("id");
+		$result = array();
+		$DBResult = null;
+		$DBResult = Model::factory($this->modelName)->getTestDetailsByTest($record_id);
+		$fieldNames = Model::factory($this->modelName)->getFieldNames();
+		foreach ($DBResult as $data)
+		{
+			$item = array();
+			foreach ($fieldNames as $fieldName) {
+				$item[$fieldName] = $data->$fieldName;
+			}
+			array_push($result, $item);
+		}
+		if (sizeof($result) < 1)
+		{
+			$result[] = array('record_id', 'null');
+		}
+		$r = json_encode($result);
+		$this->response->body($r);
+	}
 
 }
