56e1f88321efef8a1f0289a15e518075e5e6b6ae
yurkovskiy <yurkovskiy@gmail.com>
2015-09-01 12:50:34 +0300

    Big update of insertData method in Test Controller

--- application/classes/Controller/Test.php
+++ application/classes/Controller/Test.php
@@ -31,4 +31,71 @@ class Controller_Test extends Controller_BaseAdmin {
 		$r = json_encode($result);
 		$this->response->body($r);
 	}
+	
+	public function action_insertData()
+	{
+		if (!Auth::instance()->logged_in($this->ADMIN_ROLE))
+		{
+			throw new HTTP_Exception_403("You don't have permissions to insert records");
+		}
+		else
+		{
+			$result = array();
+			$model = null;
+			
+			// Read POST data in JSON format
+			$params = @json_decode(file_get_contents($this->RAW_DATA_SOURCE));
+			
+			// check if input data is given
+			if (is_null($params))
+			{
+				$model = "Error: No input data";
+			}
+			else
+			{
+				// Convert Object into Array
+				$paramsArr = get_object_vars($params);
+				$values = $paramsArr;
+				$model = Model::factory($this->modelName)->registerRecord($values);
+			}
+				
+			if (!is_string($model) && is_int($model))
+			{
+				// Creating response in JSON format
+				$result["id"] = $model;
+				$result["response"] = "ok";
+				
+				// Everything is OK, so we try to create a directory for test
+				$TESTS_BASE_DIR = DOCROOT."tests";
+				$directory_name = "test_".$model;
+				
+				$directory_name = $TESTS_BASE_DIR.DIRECTORY_SEPARATOR.$directory_name;
+												
+				$dir = @mkdir($directory_name);
+				if (!$dir)
+				{
+					$result["directory"] = "Error: directory is not created";
+				}
+				else
+				{
+					$result["directory"] = "Success: directory has been created";
+				}
+				
+			}
+			else
+			{
+				if (is_string($model))
+				{
+					$result["response"] = $model;
+				}
+				else
+				{
+					$result["response"] = "error";
+				}
+			
+			}
+			$this->response->body(json_encode($result));
+		}
+		
+	}
 }
