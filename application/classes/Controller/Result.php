6c4a0a398f59a7186908e32448eb9b4bed7962cb
yurkovskiy <yurkovskiy@gmail.com>
2015-09-02 09:53:04 +0300

    Added getResultByStudent

--- application/classes/Controller/Result.php
+++ application/classes/Controller/Result.php
@@ -14,5 +14,28 @@ class Controller_Result extends Controller_BaseAjax {
 		// nothing to do :-)
 		$this->response->body(json_decode(array("response" => "error")));
 	}
+	
+	public function action_getRecordsByStudent()
+	{
+		$record_id = $this->request->param("id");
+		$result = array();
+		$DBResult = null;
+		$DBResult = Model::factory($this->modelName)->getResultByStudent($record_id);
+		$fieldNames = Model::factory($this->modelName)->getFieldNames();
+		foreach ($DBResult as $data)
+		{
+			$item = array();
+			foreach ($fieldNames as $fieldName) {
+				$item[$fieldName] = $data->$fieldName;
+			}
+			array_push($result, $item);
+		}
+		if (sizeof($result) < 1)
+		{
+			$result[] = array('record_id', 'null');
+		}
+		$r = json_encode($result);
+		$this->response->body($r);
+	}
 
 }
