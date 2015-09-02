29b142d209cadc7d510022478bae9e42543cf46d
yurkovskiy <yurkovskiy@gmail.com>
2015-09-02 09:53:04 +0300

    Added getResultByStudent

--- application/classes/Model/Result.php
+++ application/classes/Model/Result.php
@@ -10,5 +10,11 @@ class Model_Result extends Model_Common {
 	protected $tableName = "results";
 	protected $fieldNames = array("session_id", "student_id", "test_id", "session_date", "start_time", "end_time", "result", "questions", "true_answers", "answers");
 	
+	public function getResultByStudent($student_id)
+	{
+		$query = DB::select_array($this->fieldNames)->from($this->tableName)->where($this->fieldNames[1], "=", $student_id)->order_by($this->fieldNames[0], 'asc');
+		$result = $query->as_object()->execute();
+		return $result;
+	}
 	
 }
