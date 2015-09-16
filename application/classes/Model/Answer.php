a61138ff2739f71cb5bb8a4d20b6fdc61e2c855d
yurkovskiy <yurkovskiy@gmail.com>
2015-09-16 22:03:53 +0300

    Updated Check answer algorithm. Added fully support for Simple- and Multi- choice types of questions

--- application/classes/Model/Answer.php
+++ application/classes/Model/Answer.php
@@ -15,25 +15,63 @@ class Model_Answer extends Model_Common {
 		return $this->getEntityBy($this->fieldNames[1], $question_id);
 	}
 	
-	public function checkAnswers($answer_ids)
+	public function countTrueAnswersByQuestion($question_id)
 	{
+		$query = "SELECT COUNT({$this->fieldNames[0]}) AS count 
+				FROM {$this->tableName} 
+				WHERE {$this->fieldNames[2]} = 1
+				AND {$this->fieldNames[1]} = {$question_id}";
+		$count = DB::query(Database::SELECT, $query)->execute()->get('count');
+		return $count;
+	}
+	
+	public function checkAnswers($question_id, $answer_ids)
+	{
+		$true_answers_unumber = 0;
+		$true_answers_number = 0; // by default;
 		if (!is_array($answer_ids))
 		{
 			throw new Kohana_Exception("Error input parameters");
 		}
 		else
 		{
+			// get question type
+			$question_type = Model::factory("Question")->getQuestionTypeById($question_id);
+			if ($question_type == 2)
+			{
+				$true_answers_number = $this->countTrueAnswersByQuestion($question_id);
+			}
+			
+			// get answers
 			$answers = $this->getRecordsByIds($answer_ids);
+			
 			foreach ($answers as $answer)
 			{
-				// check if incorrect
+				// check if incorect answer is present, so it's bad :-)
 				if ($answer->true_answer == 0)
 				{
 					return false;
 				}
+				else 
+				{
+					$true_answers_unumber++;
+				}
+			}
+			
+			// final check
+			// simple choice
+			if (($question_type == 1) && ($true_answers_unumber > 0))
+			{
 				return true;
 			}
-		}
+			// multi choice
+			if (($question_type == 2) && ($true_answers_unumber == $true_answers_number))
+			{
+				return true;
+			}
+			
+			return false;
+		} // else (no exception)
 	}
 	
 }
