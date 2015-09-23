ab466806340693ae35486cec36453d14a1798a3a
yurkovskiy <yurkovskiy@gmail.com>
2015-09-23 13:35:23 +0300

    Added Widget entity

---
+++ application/classes/Controller/Widget.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Widget Controller for handle AJAX requests
+ *
+ */
+
+class Controller_Widget extends Controller_BaseAdmin {
+
+	protected $modelName = "Widget";
+
+}
