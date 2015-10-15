06529d1933be5066eb30d49172ab4032164c6cbb
yurkovskiy <yurkovskiy@gmail.com>
2015-10-15 12:52:14 +0300

    Fully rewritten getTimeTablesFromNowInMonth added check user. Deleted getTimeTablesForStudent

--- application/classes/Model/TimeTable.php
+++ application/classes/Model/TimeTable.php
@@ -17,13 +17,36 @@ class Model_TimeTable extends Model_Common {
 	 * 
 	 * Return records from $this->timetable if event_date is suitable CURDATE() AND CURDATE() + 1 MONTH
 	 */
-	public function getTimeTablesFromNowInMonth()
+	public function getTimeTablesFromNowInMonth($user_id)
 	{
-		$query = DB::select_array($this->fieldNames)
+		$query = null;
+		$group_id = null;
+		
+		// check user and find out group_id
+		$student = Model::factory("Student")->getRecord($user_id);
+		foreach ($student as $record)
+		{
+			$group_id = $record->group_id;
+		}
+
+		if (is_null($group_id)) 
+		{
+			$query = DB::select_array($this->fieldNames)
 				->from($this->tableName)
 				->where($this->fieldNames[3], ">=", DB::expr("CURDATE()"))
 				->and_where($this->fieldNames[3], "<=", DB::expr("DATE_ADD(CURDATE(), INTERVAL 1 MONTH)"))
 				->order_by($this->fieldNames[0], 'asc');
+		}
+		else 
+		{
+			$query = DB::select_array($this->fieldNames)
+				->from($this->tableName)
+				->where($this->fieldNames[1], "=", $group_id)
+				->and_where($this->fieldNames[3], ">=", DB::expr("CURDATE()"))
+				->and_where($this->fieldNames[3], "<=", DB::expr("DATE_ADD(CURDATE(), INTERVAL 1 MONTH)"))
+				->order_by($this->fieldNames[0], 'asc');
+		}
+		
 		$result = $query->as_object()->execute();
 		return $result;
 	}
@@ -46,33 +69,6 @@ class Model_TimeTable extends Model_Common {
 	public function getTimeTablesForSubject($subject_id)
 	{
 		return $this->getEntityBy($this->fieldNames[2], $subject_id);		
-	}
-	
-	/**
-	 * 
-	 * @param int $student_id
-	 * @return MySQL ResultSet
-	 * 
-	 * Return records from $this->timetable if event_date is suitable CURDATE() AND CURDATE() + 1 MONTH for student
-	 */
-	public function getTimeTablesFromNowInMonthForStudent($student_id)
-	{
-		// find out group_id for student
-		$group_id = null;
-		$student = Model::factory("Student")->getRecord($student_id);
-		foreach ($student as $record)
-		{
-			$group_id = $record->group_id;
-		}
-		
-		$query = DB::select_array($this->fieldNames)
-			->from($this->tableName)
-			->where($this->fieldNames[1], "=", $group_id)
-			->and_where($this->fieldNames[3], ">=", DB::expr("CURDATE()"))
-			->and_where($this->fieldNames[3], "<=", DB::expr("DATE_ADD(CURDATE(), INTERVAL 1 MONTH)"))
-			->order_by($this->fieldNames[0], 'asc');
-		$result = $query->as_object()->execute();
-		return $result;
 	}
 	
 }
