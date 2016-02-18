c2fab77d582e1e730b1e84a764507741c30d06f5
yurkovskiy <yurkovskiy@gmail.com>
2016-02-18 20:11:02 +0200

    Fixed bug with JSON keys in login controller

--- application/classes/Controller/Login.php
+++ application/classes/Controller/Login.php
@@ -1,83 +1,89 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined ('SYSPATH') or die ('No direct script access.');
 
 /**
  * Login Controller for handle AJAX requests
- *
  */
-
-class Controller_Login extends Controller {
-	
+class Controller_Login extends Controller 
+{
 	private $RAW_DATA_SOURCE = "php://input";
-
+	
 	/**
+	 *
 	 * @name index - login action
 	 * @return JSON - with user parameters (id, name, roles)
 	 */
-	public function action_index()
+	public function action_index() 
 	{
 		$result = array();
 		$params = @json_decode(file_get_contents($this->RAW_DATA_SOURCE));
 		
-		if (is_null($params))
+		if (is_null($params)) 
 		{
 			$result["response"] = "Error: No input data";
-		}
-		else
+		} 
+		else 
 		{
-		
-			$success = Auth::instance()->login($params->username, $params->password, FALSE);
-			if ($success)
+			// check JSON keys 
+			if (property_exists($params, "username") && (property_exists($params, "password"))) 
 			{
-				// extract user's inforamtion
-				$roles = Auth::instance()->get_user()->roles->find_all();
-				foreach ($roles as $role)
+				$success = Auth::instance()->login($params->username, $params->password, FALSE);
+				if ($success) 
 				{
-					$result["roles"][] = $role->name;								
+					// extract user's inforamtion
+					$roles = Auth::instance()->get_user()->roles->find_all();
+					foreach ($roles as $role) 
+					{
+						$result["roles"][] = $role->name;
+					}
+					
+					$result["id"] = Auth::instance()->get_user()->id;
+					$result["username"] = Auth::instance()->get_user()->username;
+					$result["response"] = "ok";
+				} 
+				else 
+				{
+					$result["response"] = "Invalid login or password";
 				}
-				
-				$result["id"] = Auth::instance()->get_user()->id;
-				$result["username"] = Auth::instance()->get_user()->username;
-													
-				$result["response"] = "ok";
 			}
 			else 
 			{
-				$result["response"] = "Invalid login or password";
+				$result["response"] = "Wrong Request";
 			}
 		}
 		
 		$this->response->body(json_encode($result));
-		
 	}
 	
-	public function action_logout()
+	public function action_logout() 
 	{
 		Auth::instance()->logout(TRUE);
 	}
 	
 	/**
+	 *
 	 * @name isLogged - check if anyuser is logged
 	 * @return boolean (JSON)
 	 */
-	public function action_isLogged()
+	public function action_isLogged() 
 	{
 		$result = null;
-		if (Auth::instance()->logged_in())
+		if (Auth::instance()->logged_in()) 
 		{
 			$roles = Auth::instance()->get_user()->roles->find_all();
-			foreach ($roles as $role)
+			foreach ($roles as $role) 
 			{
 				$result["roles"][] = $role->name;
 			}
 			$result["id"] = Auth::instance()->get_user()->id;
 			$result["username"] = Auth::instance()->get_user()->username;
 			$result["response"] = "logged";
-		}
-		else
+		} 
+		else 
 		{
 			$result["response"] = "non logged";
 		}
 		$this->response->body(json_encode($result));
 	}
-
 }
