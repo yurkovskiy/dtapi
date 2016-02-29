cd9e502d3d087477a9ccb22be30fc7e07841dbe9
yurkovskiy <yurkovskiy@gmail.com>
2016-02-29 18:35:40 +0200

    Added methods getGroupsBySpeciality() and getGroupsByFaculty()

--- application/classes/Model/Group.php
+++ application/classes/Model/Group.php
@@ -10,4 +10,14 @@ class Model_Group extends Model_Common {
 	protected $tableName = "groups";
 	protected $fieldNames = array("group_id", "group_name", "speciality_id", "faculty_id");
 	
+	public function getGroupsBySpeciality($speciality_id)
+	{
+		return $this->getEntityBy($this->fieldNames[2], $speciality_id);
+	}
+	
+	public function getGroupsByFaculty($faculty_id)
+	{
+		return $this->getEntityBy($this->fieldNames[3], $faculty_id);
+	}
+	
 }
