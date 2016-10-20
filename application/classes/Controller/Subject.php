9dc634414c2912fc0717e621dba5981309ab1d39
yurkovskiy <yurkovskiy@gmail.com>
2016-10-20 07:24:24 +0300

    Added search functionality to the Subject entity

--- application/classes/Controller/Subject.php
+++ application/classes/Controller/Subject.php
@@ -9,4 +9,26 @@ class Controller_Subject extends Controller_BaseAdmin {
 	
 	protected $modelName = "Subject";
 	
+	public function action_getRecordsBySearch()
+	{
+		$criteria = $this->request->param("id");
+		$result = array();
+		$fieldNames = Model::factory($this->modelName)->getFieldNames();
+		$DBResult = Model::factory($this->modelName)->getRecordsBySearch($criteria);
+		
+		foreach ($DBResult as $data)
+		{
+			$item = array();
+			foreach ($fieldNames as $fieldName) {
+				$item[$fieldName] = $data->$fieldName;
+			}
+			array_push($result, $item);
+		}
+		if (sizeof($result) < 1)
+		{
+			$result["response"] = "no records";
+		}
+		$this->response->body(json_encode($result, JSON_UNESCAPED_UNICODE));
+	}
+	
 }
