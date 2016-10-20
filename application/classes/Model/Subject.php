adef9af298f5a536b4118e40a1a030e9af0af3ce
yurkovskiy <yurkovskiy@gmail.com>
2016-10-20 07:24:24 +0300

    Added search functionality to the Subject entity

--- application/classes/Model/Subject.php
+++ application/classes/Model/Subject.php
@@ -10,4 +10,9 @@ class Model_Subject extends Model_Common {
 	protected $tableName = "subjects";
 	protected $fieldNames = array("subject_id", "subject_name", "subject_description");
 	
+	public function getRecordsBySearch($criteria)
+	{
+		return $this->getRecordsBySearchCriteria($this->fieldNames[1], $criteria);
+	}
+	
 }
