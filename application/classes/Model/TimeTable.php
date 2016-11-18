7088dfd6ab9e1205b85fd85d304db8dab459f178
Yuriy <yurkovskiy@gmail.com>
2016-11-18 10:44:47 +0200

    [MODIFIED] Changed TimeTable Entity. Added new fields into the DB table: start_date, start_time, end_date, end_time

--- application/classes/Model/TimeTable.php
+++ application/classes/Model/TimeTable.php
@@ -8,48 +8,7 @@
 class Model_TimeTable extends Model_Common {
 	
 	protected $tableName = "timetables";
-	protected $fieldNames = array("timetable_id", "group_id","subject_id", "event_date");
-	
-	/**
-	 * @name getTimeTablesForThreeMonth
-	 * @access public
-	 * @return MySQL ResultSet
-	 * 
-	 * Return records from $this->timetable if event_date is suitable CURDATE() AND CURDATE() + 1 MONTH
-	 */
-	public function getTimeTablesFromNowInMonth($user_id)
-	{
-		$query = null;
-		$group_id = null;
-		
-		// check user and find out group_id
-		$student = Model::factory("Student")->getRecord($user_id);
-		foreach ($student as $record)
-		{
-			$group_id = $record->group_id;
-		}
-
-		if (is_null($group_id)) 
-		{
-			$query = DB::select_array($this->fieldNames)
-				->from($this->tableName)
-				->where($this->fieldNames[3], ">=", DB::expr("CURDATE()"))
-				->and_where($this->fieldNames[3], "<=", DB::expr("DATE_ADD(CURDATE(), INTERVAL 1 MONTH)"))
-				->order_by($this->fieldNames[0], 'asc');
-		}
-		else 
-		{
-			$query = DB::select_array($this->fieldNames)
-				->from($this->tableName)
-				->where($this->fieldNames[1], "=", $group_id)
-				->and_where($this->fieldNames[3], ">=", DB::expr("CURDATE()"))
-				->and_where($this->fieldNames[3], "<=", DB::expr("DATE_ADD(CURDATE(), INTERVAL 1 MONTH)"))
-				->order_by($this->fieldNames[0], 'asc');
-		}
-		
-		$result = $query->as_object()->execute();
-		return $result;
-	}
+	protected $fieldNames = array("timetable_id", "group_id","subject_id", "start_date", "start_time", "end_date", "end_time");
 	
 	/**
 	 * 
