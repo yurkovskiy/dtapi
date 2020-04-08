5f64ed6dc6a1f68b922fdd595fbf5ab6ec14f799
yurkovskiy <yurkovskiy@gmail.com>
2020-04-08 16:32:17 +0300

    [FIX] User Model added full

--- application/classes/Model/User.php
+++ application/classes/Model/User.php
@@ -2,7 +2,37 @@
 
 class Model_User extends Model_Auth_User {
 	
-	protected $_table_columns = array("id" => "", "email" => "", "username" => "", "password" => "", "logins" => "", "last_login" => "");
+	protected $_table_columns = array(
+			"id" => array(
+					"type" => "int",
+					"column_name" => "id"
+			),
+			"email" => array(
+					"type" => "string",
+					"column_name" => "email"
+			),
+			"username" => array(
+					"type" => "string",
+					"column_name" => "username"
+			),
+			"password" => array(
+					"type" => "string",
+					"column_name" => "password"
+			),
+			"logins" => array(
+					"type" => "string",
+					"column_name" => "logins"
+			),
+			"last_login" => array(
+					"type" => "string",
+					"column_name" => "last_login"
+			)
+	);
+	
+	public function list_columns()
+	{
+		return $this->_table_columns;
+	}
 	
 }
 
